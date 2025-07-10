9cab6999e8e309ec89fa077c116bb0d513845e9b
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-10 15:05:07 +0200

    [UPD] impostazione-funzione

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-const 
+const km = document.getElementById(`km`);
+const eta = document.getElementById(`eta`);
+const button = document.getElementById(`send`);
+
+button.addEventListener(`click`, function (event) {
+  //tolgo il refresh della pagina
+  event.preventDefault();
+
+  
+});
