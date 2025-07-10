1c86930485dcda84d449e9e7a06a89e127f62dba
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-10 15:10:26 +0200

    [UPD] calcolo-prezzo

--- js/script.js
+++ js/script.js
@@ -6,5 +6,10 @@ button.addEventListener(`click`, function (event) {
   //tolgo il refresh della pagina
   event.preventDefault();
 
+  //dati calcolo costo al km
+  let euroKm = document.getElementById(`km`).value * 0.21;
+  let prezzo = `${euroKm} €`
+  console.log(prezzo);
+
   
 });
